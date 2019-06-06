9782523177449b3764d5520c029e006f37ea8000
Paul Martin <communty.hivemind@gmail.com>
2019-06-06 10:27:30 +0100

    Dishwashers page should contain particular product
    Test for element with a particular `data-product-id` attribute. Item is
    currently hard-coded into the view.
    
    In this commit I also change the arrow functions in my Mocha tests into
    standard functions after seeing this recommendation in the Mocha docs:
    
    https://mochajs.org/#arrow-functions

--- test/feature-tests.test.js
+++ test/feature-tests.test.js
@@ -7,14 +7,27 @@ const expect = chai.expect
 const app = require('../app.js')
 const agent = request.agent(app)
 
-describe('Test the "dishwashers" results page', () => {
-  it('"/dishwashers" route returns the product listing page', (done) => {
+describe('Test the "dishwashers" results page', function () {
+  let $
+
+  before(function (done) {
     agent.get('/dishwashers')
-      .end((err, res) => {
-        const $ = cheerio.load(res.text)
-        const heading = $('h1')
-        expect(heading.text()).to.contain('Dishwashers')
+      .end(function (err, res) {
+        $ = cheerio.load(res.text)
         done()
       })
+  })
+
+  it('"/dishwashers" route returns the product listing page', function () {
+    const heading = $('h1')
+    expect(heading.text()).to.contain('Dishwashers')
+  })
+
+  describe('"/dishwashers" route contains details of dishwashers', function () {
+    it('contains an element with a particular data-product-id attribute', function () {
+      const productId = 1391191
+      const productElement = $(`[data-product-id=${productId}]`)
+      expect(productElement.length).to.equal(1)
+    })
   })
 })
