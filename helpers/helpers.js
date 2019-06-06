bf4e4e114ec734c4e066ba60af92f8ff181a6569
Paul Martin <communty.hivemind@gmail.com>
2019-06-06 15:31:26 +0100

    Don't run helper.mockApiDataIfNoResponse in tests
    No need to run this during the tests as the API response is mocked
    anyway, and the output was making my test output look untidy.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,10 +1,12 @@
 const helpers = {
   mockApiDataIfNoResponse: function(data) {
-    if (data.body.products === undefined) {
-      console.log("The API didn't return any data, so we'll use the saved data")
-      data.body = require('../test/mockApiResponses/dishwashers.json')
-    } else {
-      console.log("Using the API response data")
+    if (process.env.NODE_ENV != "test") {
+      if (data.body.products === undefined) {
+        console.log("The API didn't return any data, so we'll use the saved data")
+        data.body = require('../test/mockApiResponses/dishwashers.json')
+      } else {
+        console.log("Using the API response data")
+      }
     }
     return data
   }
