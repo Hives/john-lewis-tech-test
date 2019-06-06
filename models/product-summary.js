1c38e488a62d9606a213dd50d3d9826ec5cf7af4
Paul Martin <communty.hivemind@gmail.com>
2019-06-06 21:16:21 +0100

    Deal with products which have a discounted price
    Some products' price in the API response data appears as something like
    `{ from: 259.99, to: 239.99 }`, instead of just plain `239.99`. I've
    added another test case to cover this situation.

--- models/product-summary.js
+++ models/product-summary.js
@@ -2,7 +2,7 @@ class ProductSummary {
   constructor (data) {
     this.productId = data.productId
     this.title = data.title
-    this.price = data.price.now
+    this.price = data.price.now.to || data.price.now
     this.image = data.image
   }
 }
--- test/product-summary.test.js
+++ test/product-summary.test.js
@@ -5,6 +5,7 @@ const ProductSummary = require('../models/product-summary.js')
 describe('ProductSummary class', function () {
   const productData = require('./mockApiResponses/dishwashers.json').products
   const productSummary = new ProductSummary(productData[0])
+  console.log(productData[3])
 
   it('has a productId', function () {
     expect(productSummary.productId).to.equal('1391191')
@@ -20,5 +21,17 @@ describe('ProductSummary class', function () {
 
   it('has an image url', function () {
     expect(productSummary.image).to.equal('//johnlewis.scene7.com/is/image/JohnLewis/233326789?')
+  })
+
+  describe('some items have a discounted price', function () {
+    it('these ones should also have a price when converted to a ProductSummary', function () {
+      const productData = {
+        price: {
+          now: { from: '120', to: '100' }
+        }
+      }
+      const productSummary = new ProductSummary(productData)
+      expect(productSummary.price).to.equal('100')
+    })
   })
 })
