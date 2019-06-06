d2a1968a4a92246af4050fd7ab97c6d463c5a2a0
Paul Martin <communty.hivemind@gmail.com>
2019-06-06 13:17:29 +0100

    Update controller and view to use Product class
    In the controller for the /dishwashers route I now convert the API
    response into an array of Product instances which is passed through to
    the view, rather than passing through the raw API data.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const request = require('request')
+const Product = require('./models/product.js')
 
 const app = express()
 
@@ -8,15 +9,16 @@ app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
 app.get('/dishwashers', (req, res) => {
-  // res.send("hello")
   const searchTerm = 'dishwasher'
   const apiKey = 'Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb'
   const numResults = 20
-  const url = `https://api.johnlewis.com/v1/products/search?q=${searchTerm}&key=${apiKey}&pageSize=${numResults}`
+  const apiUrl = `https://api.johnlewis.com/v1/products/search?q=${searchTerm}&key=${apiKey}&pageSize=${numResults}`
 
-  request(url, { json: true }, (err, apiResponse, body) => {
+  request(apiUrl, { json: true }, (err, apiResponse, body) => {
     if (err) { return console.log(err) }
-    const productList = apiResponse.body.products
+    const productList = apiResponse.body.products.map(
+      productData => new Product(productData) 
+    )
     res.render('dishwashers', { productList: productList } )
   })
 })
