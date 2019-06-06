3fbc45da648bc5c80753e3dad507e5a1e9bbbad1
Paul Martin <communty.hivemind@gmail.com>
2019-06-06 18:12:29 +0100

    Add a test for the API key value

--- test/api-query.test.js
+++ test/api-query.test.js
@@ -5,6 +5,10 @@ const apiQuery = require('../helpers/api-query.js')
 describe('apiQuery object', function () {
   const key = apiQuery.key
 
+  it('the API key is set to "Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb"', function () {
+    expect(key).to.equal('Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb')
+  })
+
   describe('.construct method', function () {
     it('can construct the URL for a product search query', function () {
       const url = apiQuery.construct({
