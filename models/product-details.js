944199298c3b851afc25e0642c79e30d05463bd8
Paul Martin <communty.hivemind@gmail.com>
2019-06-08 09:12:40 +0100

    Complete ProductDetails class
    Has these properties:
    - productId
    - title
    - price
    - images array
    - code
    - productInformation
    - displaySpecialOffer
    - includedServices
    - attributes array

--- models/product-details.js
+++ models/product-details.js
@@ -1,6 +1,14 @@
 class ProductDetails {
   constructor (data) {
     this.productId = data.productId
+    this.title = data.title
+    this.price = data.price.now
+    this.images = data.media.images.urls
+    this.code = data.code
+    this.productInformation = data.details.productInformation
+    this.displaySpecialOffer = data.displaySpecialOffer
+    this.includedServices = data.additionalServices.includedServices
+    this.attributes = data.details.features[0].attributes
   }
 }
 
--- test/product-details.test.js
+++ test/product-details.test.js
@@ -3,10 +3,57 @@ const expect = chai.expect
 const ProductDetails = require('../models/product-details.js')
 
 describe('ProductDetails class', function () {
-  const productData = require('./mockApiResponses/product-1391191.json')
+  const productData = require('./mockApiResponses/product-2135702.json')
   const productDetails = new ProductDetails(productData)
 
   it('has a productId', function () {
-    expect(productDetails.productId).to.equal('1391191')
+    expect(productDetails.productId).to.equal('2135702')
+  })
+
+  it('has a title', function () {
+    expect(productDetails.title).to.equal("Hotpoint LTB4B019 Aquarius Integrated Dishwasher, White")
+  })
+
+  it('has a price', function () {
+    expect(productDetails.price).to.equal("219.00")
+  })
+  // need to also deal with the case where for example
+  // data.price.now == { then: 100, now, 80 }
+
+  it('has an array of images', function () {
+    expect(productDetails.images.length).to.equal(9)
+    expect(productDetails.images[8]).equal("//johnlewis.scene7.com/is/image/JohnLewis/234676712alt7?")
+  })
+  
+  it('has a product code', function () {
+    expect(productDetails.code).to.equal("88702102")
+  })
+  
+  it('has some product information', function () {
+    expect(productDetails.productInformation).to.contain('the Hotpoint LTB4B019 integrated dishwasher is a value packed washing machine')
+  })
+  
+  it('has special offer info', function () {
+    expect(productDetails.displaySpecialOffer).to.equal("Receive a 100 day money back guarantee")
+  })
+  // also need at test for when special offer info is empty?
+  
+  it('has an array of additional services', function () {
+    expect(productDetails.includedServices.length).to.equal(1)
+    expect(productDetails.includedServices[0]).to.equal("2 year guarantee included")
+  })
+  
+  it('has an array of attributes', function () {
+    expect(productDetails.attributes.length).to.equal(31)
+  })
+
+  describe('.attributes', function () {
+    it('have a name', function () {
+      expect(productDetails.attributes[1].name).to.equal("Dimensions")
+    })
+
+    it('have a value', function () {
+      expect(productDetails.attributes[1].value).to.equal("H82 x W59.5 x D57 cm")
+    })
   })
 })
